5cf64e43a2e070309f54c65884c727774d91a546
Artyom Hack <xakimullin2@gmail.com>
2024-03-13 02:58:12 +0300

    Commit add method main

--- src/com/artyom/calculator/Main.java
+++ src/com/artyom/calculator/Main.java
@@ -5,5 +5,13 @@ public class Main {
         Calculator calculator = new Calculator(input);
         return calculator.getValue();
     }
+
+    public static void main(String[] args) {
+        System.out.println(calc("1 + 3"));
+        System.out.println(calc("VI / III"));
+//        System.out.println(calc("I - II"));
+//        System.out.println(calc("I + 1"));
+        System.out.println(calc("1 + 2 + 3"));
+    }
 }
 
