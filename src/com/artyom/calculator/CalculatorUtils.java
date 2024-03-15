87aab1a20a01fd554bc10f893cd7ae967b34ec6b
Artyom Hack <xakimullin2@gmail.com>
2024-03-15 22:42:56 +0300

    Commit. Добавил остальные римские значения в Map

--- src/com/artyom/calculator/CalculatorUtils.java
+++ src/com/artyom/calculator/CalculatorUtils.java
@@ -11,6 +11,14 @@ public class CalculatorUtils {
             put(5, "V");
             put(9, "IX");
             put(10, "X");
+            put(40, "XL");
+            put(50, "L");
+            put(90, "XC");
+            put(100, "C");
+            put(400, "CD");
+            put(500, "D");
+            put(900, "CM");
+            put(1000, "M");
         }
     };
     public static final Map<String, Integer> priorityOperation = new HashMap<>() {
@@ -63,7 +71,6 @@ public class CalculatorUtils {
                 output.append(romanDictionary.get(key));
                 break;
             }
-
             output.append(romanDictionary.get(key));
             digit -= key;
         }
